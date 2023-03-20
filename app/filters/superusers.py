4b79c6593c43703b7e901fff4b3495a63bd4be24
williamqosmos <max.2898@yandex.ru>
2023-03-20 12:06:58 +0700

    Adding changes

--- app/filters/superusers.py
+++ app/filters/superusers.py
@@ -2,9 +2,12 @@ from aiogram.types import Message
 
 from aiogram.filters import BaseFilter
 
+from app.models.config.main import BotConfig
+
+
 class SuperUserFilter(BaseFilter):
-    def __init__(self, superusers: list[int]):  # [2]
-        self.superusers = superusers
+    def __init__(self, config: BotConfig):  # [2]
+        self.superusers = config.superusers
 
     async def __call__(self, message: Message) -> bool:  # [3]
         return message.from_user.id in self.superusers
--- app/handlers/superuser.py
+++ app/handlers/superuser.py
@@ -1,24 +1,17 @@
-from aiogram import Dispatcher, Bot, Router
+from aiogram import Bot, Router
 from aiogram.filters import Command
 from aiogram.types import Message
-from functools import partial
 
 from app.filters.superusers import SuperUserFilter
-from app.models.config.main import BotConfig
 
 router = Router(name=__name__)
+router.message.filter(SuperUserFilter())
 
+@router.message(Command(commands="exception"))
 async def exception(message: Message):
     raise RuntimeError(message.text)
 
 
+@router.message(Command(commands="get_out"))
 async def leave_chat(message: Message, bot: Bot):
     await bot.leave_chat(message.chat.id)
-
-
-def setup_superuser(dp: Dispatcher, bot_config: BotConfig):
-    router.message.filter(SuperUserFilter(bot_config.superusers))
-    router.message.register(exception, Command(commands="exception"))
-    router.message.register(leave_chat, Command(commands="get_out"))
-
-    dp.include_router(router)
