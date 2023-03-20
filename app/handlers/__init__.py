97f3c3fd28ed46c091a08abf53444851f98af240
williamqosmos <max.2898@yandex.ru>
2023-03-20 12:06:36 +0700

    Change registration router superuser

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -4,7 +4,7 @@ from aiogram import Dispatcher
 
 from app.handlers.base import router as base
 from app.handlers.errors import setup_errors
-from app.handlers.superuser import setup_superuser
+from app.handlers.superuser import router as superuser
 from app.models.config.main import BotConfig
 
 logger = logging.getLogger(__name__)
@@ -13,5 +13,5 @@ logger = logging.getLogger(__name__)
 def setup_handlers(dp: Dispatcher, bot_config: BotConfig):
     setup_errors(dp, bot_config.log_chat)
     dp.include_router(base)
-    setup_superuser(dp, bot_config)
+    dp.include_router(superuser)
     logger.debug("handlers configured successfully")
